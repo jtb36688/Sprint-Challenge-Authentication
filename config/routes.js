08a1abc65e0fd3a852fcf26030bf94d1d6388da8
Jacob Bryan <jtb36688@gmail.com>
2019-03-01 11:55:32 -0500

    Finished register and login routes

--- config/routes.js
+++ config/routes.js
@@ -1,37 +1,54 @@
-const axios = require('axios');
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const db = require("../database/users-module");
 
-const { authenticate } = require('../auth/authenticate');
+const auth = require("../auth/authenticate");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", auth.authenticate, getJokes);
 };
 
 function register(req, res) {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10);
-  user.password = hash
-  
+  user.password = hash;
+  db.add(user)
+    .then(added => {
+      res.status(201).json(added);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 }
 
 function login(req, res) {
-  // implement user login
+  const { username, password } = req.body;
+  db.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = auth.makejwt(user);
+        res.status(200).json({ message: `welcome ${user.username}`, token });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    });
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
